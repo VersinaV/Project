c5838d2b0bf5343d0ffcda73204756765a3783bd
Versina Vasile <ipvvg4@gmail.com>
2024-02-06 18:06:00 +0100

    add exchengeHistori date

--- src/CurrencyExchange/ExchangeHistory.java
+++ src/CurrencyExchange/ExchangeHistory.java
@@ -40,22 +40,31 @@ public class ExchangeHistory {
     } else {
       System.out.println("История обменов: ");
       for (Exchange exchange : exchangeHistory) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date currentDate = new Date();
+        String formattedDate = sdf.format(currentDate);
         String exchangeInfo = String.format("Сумма: %.2f %s обменяли в %.2f %s%n",
             exchange.getAmount(), exchange.getFromCurrency(),
             UserInterface.exchangeCurrency(exchange.getAmount(),
                 exchange.getFromCurrency(), exchange.getToCurrency()),
             exchange.getToCurrency());
-        System.out.print(exchange.getDateTime() + " ");
+        System.out.println(formattedDate);
         System.out.println(exchangeInfo);
 
         writeToFile(exchangeInfo);
       }
     }
   }
+  private String getFormattedDate() {
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    Date currentDate = new Date();
+    return sdf.format(currentDate);
+  }
 
   private void writeToFile(String exchangeInfo) {
     try (BufferedWriter bw = new BufferedWriter(new FileWriter(file, true))) {
-      bw.write(exchangeInfo);
+      String formattedDate = getFormattedDate();
+      bw.write(formattedDate + " " + exchangeInfo);
       bw.newLine();
     } catch (IOException e) {
       e.printStackTrace();
